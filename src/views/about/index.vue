5ae785e2099bcf78ca38eb56528b8b0fde4ef0f7
yeyuhang <yyh3531@163.com>
2024-05-22 17:37:00 +0800

    feat(api):new

--- src/views/about/index.vue
+++ src/views/about/index.vue
@@ -1,12 +1,27 @@
 <script setup lang="ts">
+import { reactive } from 'vue';
+
+const ipInfo = reactive({
+  ip: undefined,
+  info: {}
+})
+fetch('https://api.vvhan.com/api/ipInfo').then(async res => {
+  let result = await res.json()
+  ipInfo.info = result.info
+  ipInfo.ip = result.ip
+})
+
 </script>
 
 <template>
-  <div>
-    <RouterLink  to="/dashboard">首页</RouterLink >
+  <div class="text-center w-screen	h-screen leading-10 py-10 bg-zinc-300">
+    <RouterLink to="/dashboard" class="text-sky-500 underline fixed bottom-10 right-10">去首页</RouterLink>
+        <div v-for="(v, k) in ipInfo.info ">
+          {{ k }}: {{ v }}
+        </div>
+
+        <div v-if="ipInfo.ip">ip: {{ ipInfo.ip }}</div>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,30 +1,19 @@
 <script setup lang="ts">
-import { reactive } from 'vue';
+import { ref } from 'vue';
 
-
-const ipInfo = reactive({
-    ip: undefined,
-    info: {}
-})
-fetch('https://api.vvhan.com/api/ipInfo').then(async res => {
+const content = ref('')
+fetch('https://api.vvhan.com/api/text/love?type=json').then(async res => {
   let result = await res.json()
-  ipInfo.info = result.info
-  ipInfo.ip = result.ip
+  content.value = result.data.content
 })
 
 </script>
 
 <template>
-  <div>
-    <RouterLink  to="/about">关于</RouterLink >
-      <div v-for="(v,k) in ipInfo.info ">
-        {{ k}}: {{ v }}
-      </div>
-       
-      <div v-if="ipInfo.ip">ip: {{ ipInfo.ip }}</div>
+  <div class="text-center w-screen	h-screen	leading-10  py-10 bg-zinc-300">
+    <RouterLink to="/about" class="text-sky-500 underline fixed bottom-10 right-10">去关于</RouterLink>
+    <h2>{{ content }}</h2>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
