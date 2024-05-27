27ddd735c3ef798df7f68e21efdbfef42b68215c
eug <yyh3531@163.com>
2024-05-28 00:33:09 +0800

    feat(music): init

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -2,7 +2,7 @@
  * @Author       : eug yyh3531@163.com
  * @Date         : 2024-05-22 22:05:28
  * @LastEditors  : eug yyh3531@163.com
- * @LastEditTime : 2024-05-26 23:57:31
+ * @LastEditTime : 2024-05-27 23:30:21
  * @FilePath     : /eug620.github.io/src/router/routes.ts
  * @Description  : filename
  * 
@@ -33,6 +33,18 @@ export const menus = [
       icon: "",
     },
     component: () => import("@/views/Movie.vue"),
+  },
+  {
+    path: "Music",
+    name: "Music",
+    meta: {
+      keepAlive: false,
+      title: "音乐",
+      auth: true,
+      affix: true,
+      icon: "",
+    },
+    component: () => import("@/views/Music.vue"),
   },
   {
     path: "Three",
---
+++ src/views/Music.vue
@@ -0,0 +1,84 @@
+<!--
+ * @Author       : eug yyh3531@163.com
+ * @Date         : 2024-05-27 23:30:13
+ * @LastEditors  : eug yyh3531@163.com
+ * @LastEditTime : 2024-05-28 00:30:55
+ * @FilePath     : /eug620.github.io/src/views/Music.vue
+ * @Description  : filename
+ * 
+ * Copyright (c) 2024 by ${git_name_email}, All Rights Reserved. 
+-->
+<template>
+    <div class="py-20 text-center">
+        <div class="text-2xl">{{ MusicInfo.name }}</div>
+        <div class="text-xs my-3">{{ MusicInfo.auther }}</div>
+        <img :src="MusicInfo.picUrl" class="w-80 mx-auto rounded-full" :class="[rotate && 'animation-rotate']" alt="">
+        <!-- <audio :src="MusicInfo.url" autoplay></audio> -->
+        <audio autoplay controls v-if="MusicInfo.url" class="mx-auto" @playing="usePlaying" @pause="usePause"
+            @ended="useGetMusic">
+            <source :src="MusicInfo.url" type="audio/mp3">
+        </audio>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { reactive, ref, nextTick } from "vue";
+interface TypeMusicInfo {
+    auther: string;
+    id: string;
+    name: string;
+    picUrl: string;
+    url: string;
+}
+const MusicInfo = reactive<TypeMusicInfo>({
+    id: '',
+    name: '',
+    auther: '',
+    picUrl: '',
+    url: '',
+})
+const useGetMusic = () => {
+    MusicInfo.url = ''
+    fetch('https://api.vvhan.com/api/wyMusic/%E7%83%AD%E6%AD%8C%E6%A6%9C?type=json').then(async res => {
+        let result: { info: TypeMusicInfo } = await res.json()
+        console.log(result);
+        MusicInfo.auther = result.info.auther
+        MusicInfo.id = result.info.id
+        MusicInfo.name = result.info.name
+        MusicInfo.picUrl = result.info.picUrl
+        MusicInfo.url = result.info.url
+    })
+}
+useGetMusic()
+
+const rotate = ref(false)
+const usePlaying = () => {
+    rotate.value = true
+
+}
+const usePause = () => {
+    rotate.value = false
+
+}
+
+</script>
+
+<style>
+@keyframes RotateZ {
+    0% {
+        transform: rotateZ(0deg);
+    }
+
+    50% {
+        transform: rotateZ(180deg);
+    }
+
+    100% {
+        transform: rotateZ(360deg);
+    }
+}
+
+.animation-rotate {
+    animation: RotateZ 10s linear infinite;
+}
+</style>
