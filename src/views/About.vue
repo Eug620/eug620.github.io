fa2c0d10164eae41b63f20a0db9f1fce92b531b9
eug <yyh3531@163.com>
2024-06-11 20:26:51 +0800

    feat(bugs): fix

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,14 +2,13 @@
  * @Author       : eug yyh3531@163.com
  * @Date         : 2024-05-22 22:05:28
  * @LastEditors  : eug yyh3531@163.com
- * @LastEditTime : 2024-06-09 01:32:03
+ * @LastEditTime : 2024-06-09 18:27:55
  * @FilePath     : /eug620.github.io/src/views/About.vue
  * @Description  : filename
  * 
  * Copyright (c) 2024 by ${git_name_email}, All Rights Reserved. 
 -->
 <script setup lang="ts">
-import { reactive } from 'vue';
 
 
 </script>
