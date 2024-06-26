930d6c4a7447020602e63023a4d976333fc6dc32
yeyuhang <yyh3531@163.com>
2024-06-26 18:21:57 +0800

    feat(secrecy): date

--- src/views/Secrecy.vue
+++ src/views/Secrecy.vue
@@ -4,71 +4,102 @@
         <div class="flex flex-col gap-3">
             <input type="text" v-model="dbTitle" placeholder="请输入标题" :disabled="status">
             <input type="text" v-model="dbPassworld" placeholder="请输入访问密码">
-            <textarea v-model="dbContent" rows="6" :placeholder="status ? '解析可查看存储内容' :'请输入存储内容'" :disabled="status" />
+            <textarea v-model="dbContent" rows="6" :placeholder="status ? '解析可查看存储内容' : '请输入存储内容'" :disabled="status" />
             <div class="text-center ">
                 <button @click="useSubmit" class="px-6 py-2  rounded-lg  bg-slate-200 "
-                :class="[(!dbTitle || !dbPassworld || !dbContent) && !status ? 'cursor-not-allowed' : 'cursor-pointer']"
-                :disabled="(!dbTitle || !dbPassworld || !dbContent) && !status">{{ status ? '解析' : '存储' }}</button>
-                <button @click="useCancel" class="px-6 py-2 ml-4 rounded-lg  bg-slate-200 cursor-pointer"
-                v-if="status"
-                >取消</button>
+                    :class="[(!dbTitle || !dbPassworld || !dbContent) && !status ? 'cursor-not-allowed' : 'cursor-pointer']"
+                    :disabled="(!dbTitle || !dbPassworld || !dbContent) && !status">{{ status ? '解析' : '存储' }}</button>
+                    <button @click="useNewSave" class="px-6 py-2 ml-4 rounded-lg  bg-slate-200 cursor-pointer"
+                    v-if="dbID && !status">另存</button>
+                    <button @click="useCancel" class="px-6 py-2 ml-4 rounded-lg  bg-slate-200 cursor-pointer"
+                    >{{status ? '取消' : '清空'}}</button>
             </div>
-           
+
         </div>
 
         <div class="gap-2 flex flex-col my-2 cursor-pointer">
             <div @click="useGetContent(s)" class="bg-slate-200 rounded-lg p-2" v-for="s in secrecy" :key="s">
-                {{ s }}
+                {{ s?.key}}
+                <span class=" float-right">{{ dayjs(s?.timeStamp).format('YYYY-MM-DD HH:mm:ss') }} </span>
             </div>
         </div>
     </div>
 </template>
 
 <script lang="ts" setup>
+import { v4 as uuidv4 } from 'uuid'
+import dayjs from 'dayjs'
 import { useCryptoStore } from '@/store/modules/crypto';
 import { useDBStore } from '@/store/modules/database'
 import { ref } from 'vue';
 const Crypto = useCryptoStore()
 const db = useDBStore()
 const dbTitle = ref('')
+const dbID = ref('')
 const dbPassworld = ref('')
 const dbContent = ref('')
 const status = ref(false)
 const secrecy = ref<any[]>([])
-const useGetSecrecy = () => {
-    db.secrecy.keys().then(RES => {
-        if (RES) secrecy.value = RES
-    })
+const newData = ref(false)
+const useGetSecrecy = async () => {
+    try {
+        const keys = await db.secrecy.keys()
+        secrecy.value = await Promise.all(keys.map((id) => {
+            return db.secrecy.getItem(id)
+        }))
+    } catch (_) {
+        db.secrecy.clear()
+    }
 }
 useGetSecrecy()
+const useNewSave= () => {
+    newData.value = true
+    useSubmit()
+}
 const useSubmit = async () => {
     console.log('submit');
     if (status.value) {
         try {
-            const content: string = await db.secrecy.getItem(dbTitle.value) || ''
-            dbContent.value = Crypto.decrypt(content, dbPassworld.value)
+            const content: any = await db.secrecy.getItem(dbID.value)
+            dbContent.value = Crypto.decrypt(content?.value, dbPassworld.value)
             status.value = false
         } catch (_) {
             alert('密码错误，解析失败！')
         }
     } else {
-        await db.secrecy.setItem(dbTitle.value, Crypto.encrypt(dbContent.value, dbPassworld.value))
-        dbTitle.value = ''
-        dbPassworld.value = ''
-        dbContent.value = ''
+        if (dbID.value && !newData.value) {
+            await db.secrecy.setItem(dbID.value, {
+                id: dbID.value,
+                key: dbTitle.value,
+                value: Crypto.encrypt(dbContent.value, dbPassworld.value),
+                timeStamp: Date.now()
+            })
+        } else {
+            const id = uuidv4()
+            await db.secrecy.setItem(id, {
+                id,
+                key: dbTitle.value,
+                value: Crypto.encrypt(dbContent.value, dbPassworld.value),
+                timeStamp: Date.now()
+            })
+        }
+        useCancel()
         useGetSecrecy()
     }
 }
 
-const useGetContent = (s: string) => {
-    dbTitle.value = s
+const useGetContent = (s: any) => {
+    dbID.value = s?.id
+    dbTitle.value = s?.key
     dbPassworld.value = ''
     dbContent.value = ''
     status.value = true
 }
 
 const useCancel = () => {
+    dbID.value = ''
     status.value = false
+    newData.value = false
     dbTitle.value = ''
     dbPassworld.value = ''
     dbContent.value = ''
