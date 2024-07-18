1b93664afa2139fef9f1eed6d31939632a40a5ae
yeyuhang <yyh3531@163.com>
2024-07-18 11:43:11 +0800

    feat(装备): 页面

--- src/views/LOL.vue
+++ src/views/LOL.vue
@@ -4,81 +4,102 @@
             <div v-if="heroList.version" class="text-center pb-6">{{ heroList.fileTime }} - Version{{ heroList.version
                 }}
             </div>
-            <template v-if="!isShowDetails">
-                <div v-for="hero in heroList.hero" :key="hero.heroId"
-                    class="my-3 bg-slate-200 p-3 flex rounded-lg cursor-pointer" @click="useHeroDetails(hero)">
-                    <img :src="`//game.gtimg.cn/images/lol/act/img/skinloading/${hero.instance_id}.jpg`"
-                        class="h-24     mr-2" alt="">
+            <div class="flex my-2 gap-2">
+                <div :class="{'bg-sky-200' : activeTab === tabs.hero  }" class="px-4 py-2 bg-slate-200 rounded-lg cursor-pointer" @click="activeTab = tabs.hero">英雄</div>
+                <div :class="{'bg-sky-200' : activeTab === tabs.equip  }" class="px-4 py-2 bg-slate-200 rounded-lg cursor-pointer" @click="activeTab = tabs.equip">装备</div>
+            </div>
+            <div v-if="!isShowDetails" >
+                <div v-if="activeTab === tabs.hero" class="grid gap-4 grid-cols-3">
+                    <div v-for="hero in heroList.hero" :key="hero.heroId"
+                        class="bg-slate-200 p-3 flex rounded-lg  cursor-pointer relative" @click="useHeroDetails(hero)">
+                        <img :src="`//game.gtimg.cn/images/lol/act/img/skinloading/${hero.instance_id}.jpg`"
+                            class="h-24     mr-2" alt="">
 
-                    <div class="flex-1">
-                        <span>{{ hero.name }}</span>
-                        <span class="text-xs pl-3">{{ hero.title }}</span>
-                        <span class="text-xs pl-3">{{ hero.alias }}</span>
-                        <div class="text-xs mt-2">
-                            定位：<span v-for=" roles in hero.roles " class="mr-2" :key="roles">{{ rolesType[roles]
-                                }}</span>
+                        <div class="flex-1">
+                            <span>{{ hero.name }}</span>
+                            <span class="text-xs pl-3">{{ hero.title }}</span>
+                            <span class="text-xs pl-3">{{ hero.alias }}</span>
+                            <div class="text-xs mt-2">
+                                定位：<span v-for=" roles in hero.roles " class="mr-2" :key="roles">{{ rolesType[roles]
+                                    }}</span>
+                            </div>
+                            <div class="text-xs mt-1">
+                                价格：{{ hero.goldPrice }} / {{ hero.couponPrice }}
+                            </div>
+                            <div class="text-xs mt-1">
+                                <span class="mr-4">攻击：{{ hero.attack }}</span>
+                                <span class="mr-4">防御：{{ hero.defense }}</span>
+                                <span class="mr-4">魔法：{{ hero.magic }}</span>
+                                <span class="mr-4">难度：{{ hero.difficulty }}</span>
+                            </div>
+                            <div v-if="hero.isWeekFree === '1'">周免</div>
                         </div>
-                        <div class="text-xs mt-1">
-                            价格：{{ hero.goldPrice }} / {{ hero.couponPrice }}
-                        </div>
-                        <div class="text-xs mt-1">
-                            <span class="mr-4">攻击：{{ hero.attack }}</span>
-                            <span class="mr-4">防御：{{ hero.defense }}</span>
-                            <span class="mr-4">魔法：{{ hero.magic }}</span>
-                            <span class="mr-4">难度：{{ hero.difficulty }}</span>
-                        </div>
-                        <div v-if="hero.isWeekFree === '1'">周免</div>
+
+                        <span
+                            :class="[hero.changeLabel === '改动英雄' && 'text-red-500', 'absolute text-xs top-1 right-1']">{{
+                            hero.changeLabel }}</span>
                     </div>
-
-                    <span :class="[hero.changeLabel === '改动英雄' && 'text-red-500']">{{ hero.changeLabel }}</span>
                 </div>
-            </template>
+                <div v-if="activeTab === tabs.equip" class="grid gap-4 grid-cols-5">
+                    <div v-for="equip in equipList.items" :key="equip.itemId" class="flex bg-slate-200 rounded-lg p-2" @click="useEquipClick(equip)">
+                        <img :src="equip.iconPath" class="w-10 h-10"alt="">
+                        <div class="ml-2">
+                            <div>{{ equip.name }}</div>
+                            <div class="text-xs">合成：{{ equip.price }}</div>
+                            <div class="text-xs">出售：{{ equip.sell }}</div>
+                            <div class="text-xs">总价：{{ equip.total }}</div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
 
             <div v-else>
                 <div>
-                    <div class="flex mb-2">
-                        <img :src="heroDetails.spells?.passive?.abilityIconPath" class="mr-4" alt="">
+                    <div class="flex my-2 items-center">
+                        <img :src="heroDetails.spells?.passive?.abilityIconPath" class="mr-4 w-20 h-20 rounded-lg"
+                            alt="">
                         <div>
                             <div>{{ heroDetails.spells?.passive?.name }}</div>
                             <div class="text-xs">{{ heroDetails.spells?.passive?.description }}</div>
                         </div>
                     </div>
-                    <div class="flex mb-2">
-                        <img :src="heroDetails.spells?.q?.abilityIconPath" class="mr-4" alt="">
+                    <div class="flex my-2 items-center">
+                        <img :src="heroDetails.spells?.q?.abilityIconPath" class="mr-4 w-20 h-20 rounded-lg" alt="">
                         <div>
                             <div>Q：{{ heroDetails.spells?.q?.name }} - <span class="text-xs">{{
-                                    heroDetails.spells?.q?.costburn
-                                    }}</span></div>
+                                heroDetails.spells?.q?.costburn
+                            }}</span></div>
                             <div class="text-xs">{{ heroDetails.spells?.q?.description }}</div>
                         </div>
 
                     </div>
-                    <div class="flex mb-2">
-                        <img :src="heroDetails.spells?.w?.abilityIconPath" class="mr-4" alt="">
+                    <div class="flex my-2 items-center">
+                        <img :src="heroDetails.spells?.w?.abilityIconPath" class="mr-4 w-20 h-20 rounded-lg" alt="">
                         <div>
                             <div>W：{{ heroDetails.spells?.w?.name }} - <span class="text-xs">{{
-                                    heroDetails.spells?.w?.costburn
-                                    }}</span></div>
+                                heroDetails.spells?.w?.costburn
+                            }}</span></div>
                             <div class="text-xs">{{ heroDetails.spells?.w?.description }}</div>
                         </div>
 
                     </div>
-                    <div class="flex mb-2">
-                        <img :src="heroDetails.spells?.e?.abilityIconPath" class="mr-4" alt="">
+                    <div class="flex my-2 items-center">
+                        <img :src="heroDetails.spells?.e?.abilityIconPath" class="mr-4 w-20 h-20 rounded-lg" alt="">
                         <div>
                             <div>E：{{ heroDetails.spells?.e?.name }} - <span class="text-xs">{{
-                                    heroDetails.spells?.e?.costburn
-                                    }}</span></div>
+                                heroDetails.spells?.e?.costburn
+                            }}</span></div>
                             <div class="text-xs">{{ heroDetails.spells?.e?.description }}</div>
                         </div>
 
                     </div>
-                    <div class="flex mb-2">
-                        <img :src="heroDetails.spells?.r?.abilityIconPath" class="mr-4" alt="">
+                    <div class="flex my-2 items-center">
+                        <img :src="heroDetails.spells?.r?.abilityIconPath" class="mr-4 w-20 h-20 rounded-lg" alt="">
                         <div>
                             <div>R：{{ heroDetails.spells?.r?.name }} - <span class="text-xs">{{
-                                    heroDetails.spells?.r?.costburn
-                                    }}</span></div>
+                                heroDetails.spells?.r?.costburn
+                            }}</span></div>
                             <div class="text-xs">{{ heroDetails.spells?.r?.description }}</div>
                         </div>
 
@@ -124,10 +145,36 @@ interface Hero {
     isWeekFree: string
     changeLabel: string
 }
+interface Items {
+    description: string
+    from: string[]
+    iconPath: string
+    into: string[]//
+    itemId: string
+    keywords: string
+    maps: string[]
+    name: string
+    plaintext: string
+    price: string
+    sell: string
+    suitHeroId: string
+    tag: string
+    total: string
+    types: string[]
+    
+}
+
 interface HeroTypes {
     fileName: string
     fileTime: string
     hero: Hero[]
+    version: string
+}
+interface EquipTypes {
+    fileName: string
+    fileTime: string
+    items: Items[]
+    tree: any[],
     version: string
 }
 interface Skin {
@@ -148,6 +195,15 @@ const heroList = reactive<HeroTypes>({
     hero: [],
     version: ''
 })
+const equipList = reactive<EquipTypes>({
+    fileName: '',
+    fileTime: '',
+    items: [],
+    tree:[],
+    version: ''
+})
+const equipMaps = new Map< string, Items>()
+
 const isShowDetails = ref(false)
 const heroDetails = reactive<HeroDetails>({
     fileName: '',
@@ -165,7 +221,11 @@ enum rolesType {
     assassin = '刺客',
     marksman = '射手',
 }
-
+enum tabs {
+    hero = 'hero',
+    equip = 'equip'
+}
+const activeTab = ref<tabs>(tabs.hero)
 const comSkins = computed(() => {
     return heroDetails.skins.filter(v => v?.chromas == '0')
 })
@@ -191,7 +251,20 @@ const useGetHeroList = () => {
 }
 useGetHeroList()
 
-
+const useGetEquipList = () => {
+    fetch(`https://game.gtimg.cn/images/lol/act/img/js/items/items.js`).then(async res => {
+        const { fileName, fileTime, items,tree, version } = await res.json()
+        equipList.fileName = fileName
+        equipList.fileTime = fileTime
+        equipList.version = version
+        equipList.items = items
+        equipList.tree = tree
+        items.forEach((v: Items) => {
+            equipMaps.set(v.itemId, v)
+        })
+    })
+}
+useGetEquipList()
 const useHeroDetails = (hero: Hero) => {
     currentIdx.value = 0
     fetch(`https://game.gtimg.cn/images/lol/act/img/js/hero/${hero.heroId}.js`).then(async res => {
@@ -207,6 +280,15 @@ const useHeroDetails = (hero: Hero) => {
         });
 
     })
+}
+
+const useEquipClick = (equip:Items) => {
+    alert(`
+模式：${equip.maps.join(' ')}\n
+装备效果：${equip.description.replace(/<[^>]+>/g, "")}\n
+合成路线：${equip.from.map(v => equipMaps.get(v)?.name)}\n
+可合成：${equip.into.map(v => equipMaps.get(v)?.name)}\n
+    `)
 }
 </script>
 
