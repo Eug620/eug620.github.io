d089dbd5123bf630723e5664ab75b9877409c703
yeyuhang <yyh3531@163.com>
2024-11-20 14:00:18 +0800

    feat: 🎸 Excalidraw

--- src/components/Footers.vue
+++ src/components/Footers.vue
@@ -1,11 +1,22 @@
 <template>
-    <div class="footer flex gap-4">
+    <div class="footer flex flex-wrap justify-center gap-4">
         <div v-for="rt in information" :key="rt.path" class="cursor-pointer">
             <router-link :to="rt.path">{{ rt.meta.title }}</router-link>
+        </div>
+        <div v-for="m in menus" :key="m.path" class="cursor-pointer">
+            <a :href="m.path">{{ m.name }}</a>
         </div>
     </div>
 </template>
 <script lang="ts" setup>
+import { ref } from 'vue'
 import { information } from '@/router/routes'
+
+const menus = ref([
+    {
+        path: 'https://eug620.github.io/micro-react/#/excalidraw',
+        name: 'Excalidraw'
+    }
+])
 </script>
 <style></style>
